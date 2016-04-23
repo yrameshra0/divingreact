8614446af3e0a303200b7b75b0489de83dee52b9
Ramesh <Ramesh@Rameshs-MacBook-Pro.local>
2016-04-23 11:46:32 +0530

    Basic react formatting changes

--- basic-react/src/js/client.js
+++ basic-react/src/js/client.js
@@ -1,12 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 class Layout extends React.Component {
     render() {
-      console.log('THIS IS TO CHECK THE WORKING');
-      return ( < h1 > It is working, Superb < /h1>);
-      }
+        return (<h1> It is working, Superb </h1>);
     }
+}
 
-    const app = document.getElementById('app');
-    ReactDOM.render( < Layout / > , app);
+const app = document.getElementById('app');
+ReactDOM.render(<Layout/>, app);
