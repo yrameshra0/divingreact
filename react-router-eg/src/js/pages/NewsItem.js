e5e1eb576d945938eeff46a4df7f159090a41651
Ramesh <Ramesh@Rameshs-MacBook-Pro.local>
2016-05-12 19:34:28 +0530

    Uses React without the class construct in more Javascript syntax

--- react-router-eg/src/js/pages/NewsItem.js
+++ react-router-eg/src/js/pages/NewsItem.js
@@ -1,14 +1,10 @@
 import React from 'react';
 
-export default class Featured extends React.Component {
-    render() {
-        return (
-            <div class='well'>
-			        <h1> {this.props.title}</h1>
+export default function Featured(props) {
+    return <div class='well'>
+			        <h1> {props.title} </h1>
 			        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Aenean lacinia bibendum nulla sed consectetur.</p>
 			        <button>More Info</button>
 			        <p/>
-		      </div>
-            );
-    }
+		      </div>;
 }
