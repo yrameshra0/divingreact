161805eb0d2384f5c3a3028741b93cfd16a624dc
Ramesh <yrameshrao@gmail.com>
2016-11-29 06:01:31 +0530

    Hooking up search bar component into App function component
    App component enabling the rendering of the SearchBar component by
    hooking up it into its layout

--- udemy/ReduxSimpleStarter/src/index.js
+++ udemy/ReduxSimpleStarter/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './youtube_page/search_bar';
+
+const YOUTUBE_API_KEY = 'AIzaSyCNGp-hXwNcwPe0rusuvBr6Vdy4OhqwWk8';
 
 // Creating a new component. This component should produce some HTML
 const App = () => {
-    return <div> Hi! </div>;
+    return (
+        <div> 
+    		<SearchBar />
+     	</div>
+        );
 };
 
 // Take this component's generated HTML and put it on the page (in the DOM)
