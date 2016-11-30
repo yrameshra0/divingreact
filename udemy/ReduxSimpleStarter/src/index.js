c8d9e412be58fe9c7ad28208e1f750031f5948ea
Ramesh <yrameshrao@gmail.com>
2016-11-30 20:51:11 +0530

    Setting up downward data flow in index App component
    App component is most parent component and many components are basing
    themselves over the youtube search api setting up search functionality
    in App component.
    
    Refactoring App component from functional to class based component.

--- udemy/ReduxSimpleStarter/src/index.js
+++ udemy/ReduxSimpleStarter/src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './youtube_page/search_bar';
+import YTSearch from 'youtube-api-search';
 
 const YOUTUBE_API_KEY = 'AIzaSyCNGp-hXwNcwPe0rusuvBr6Vdy4OhqwWk8';
 
-// Creating a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div> 
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: []
+        };
+
+        YTSearch({
+            key: YOUTUBE_API_KEY,
+            term: 'surfboards'
+        }, videos => this.setState({
+            videos
+        }));
+    }
+
+    render() {
+        return (
+            <div> 
     		<SearchBar />
      	</div>
-        );
-};
+            );
+    }
+}
 
 // Take this component's generated HTML and put it on the page (in the DOM)
 
