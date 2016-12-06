5fb5e48be24a6b77793a9d801be3bac225d22f9c
Ramesh <yrameshrao@gmail.com>
2016-12-06 18:51:45 +0530

    Searching hooked up E2E :)

--- udemy/ReduxSimpleStarter/src/index.js
+++ udemy/ReduxSimpleStarter/src/index.js
@@ -16,9 +16,13 @@ class App extends Component {
             selectedVideo: null
         };
 
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
         YTSearch({
             key: YOUTUBE_API_KEY,
-            term: 'surfboards'
+            term: term
         }, videos => this.setState({
             videos: videos,
             selectedVideo: videos[0]
@@ -28,7 +32,7 @@ class App extends Component {
     render() {
         return (
             <div> 
-    		<SearchBar />
+    		<SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
     		<VideoDetail video={this.state.selectedVideo} />
     		<VideoList onVideoSelect={selectedVideo => this.setState({
                 selectedVideo
--- udemy/ReduxSimpleStarter/src/youtube_page/search_bar.js
+++ udemy/ReduxSimpleStarter/src/youtube_page/search_bar.js
@@ -16,11 +16,16 @@ class SerachBar extends Component {
             <div className='search-bar'>
             <input
             value = {this.state.term}
-            onChange={event => this.setState({
-                term: event.target.value
-            })} />
+            onChange={event => this.onInputChange(event.target.value)} />
             </div>
             );
+    }
+
+    onInputChange(term) {
+        this.setState({
+            term
+        });
+        this.props.onSearchTermChange(term);
     }
 }
 
