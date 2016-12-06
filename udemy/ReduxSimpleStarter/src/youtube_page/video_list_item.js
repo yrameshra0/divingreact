d496304e146d41425d86a9cb057a8137d17242fb
Ramesh <yrameshrao@gmail.com>
2016-12-06 18:19:27 +0530

    Video list item click handler and data propogation added E2E

--- udemy/ReduxSimpleStarter/src/youtube_page/video_list_item.js
+++ udemy/ReduxSimpleStarter/src/youtube_page/video_list_item.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
     const snippet = video.snippet,
         imageUrl = snippet.thumbnails.default.url,
         title = snippet.title;
     return (
-        <li className='list-group-item'>
+        <li onClick={() => onVideoSelect(video)} className='list-group-item'>
 	        <div className='video-list media'>
 	        	<div className='media-left'>
 	        		<img className='media-object' src={imageUrl}/>
