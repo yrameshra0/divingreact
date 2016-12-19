1a7dbb0ed5c6178076de85f2dff4ee4b6b5c0988
Ramesh <yrameshrao@gmail.com>
2016-12-20 04:54:44 +0530

    Moving redux simple starter contents to react starter ;)

---
+++ udemy/ReactStarter/src/index.js
@@ -0,0 +1,52 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './youtube_page/search_bar';
+import VideoList from './youtube_page/video_list';
+import YTSearch from 'youtube-api-search';
+import VideoDetail from './youtube_page/video_detail';
+
+const YOUTUBE_API_KEY = 'AIzaSyCNGp-hXwNcwPe0rusuvBr6Vdy4OhqwWk8';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
+        YTSearch({
+            key: YOUTUBE_API_KEY,
+            term: term
+        }, videos => this.setState({
+            videos: videos,
+            selectedVideo: videos[0]
+        }));
+    }
+
+    render() {
+        const videoSearch = _.debounce((term) => {
+            this.videoSearch(term);
+        }, 300);
+
+        return (
+            <div> 
+    		<SearchBar onSearchTermChange={videoSearch}/>
+    		<VideoDetail video={this.state.selectedVideo} />
+    		<VideoList onVideoSelect={selectedVideo => this.setState({
+                selectedVideo
+            })} videos={this.state.videos} />
+     	</div>
+            );
+    }
+}
+
+// Take this component's generated HTML and put it on the page (in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
---
+++ udemy/ReactStarter/src/youtube_page/video_list_item.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const VideoListItem = ({video, onVideoSelect}) => {
+    const snippet = video.snippet,
+        imageUrl = snippet.thumbnails.default.url,
+        title = snippet.title;
+    return (
+        <li onClick={() => onVideoSelect(video)} className='video-item list-group-item'>
+	        <div className='video-list media'>
+	        	<div className='media-left'>
+	        		<img className='media-object' src={imageUrl}/>
+	        	</div>
+		        <div className='media-body media-right'>
+		        	<div className='media-heading'>{title}</div>
+		        </div>
+	        </div>
+        </li>
+        );
+};
+
+export default VideoListItem;
