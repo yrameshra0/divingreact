4a6e307d4a70c58c101e461f9d13409c67bb81d1
Ramesh <yrameshrao@gmail.com>
2016-12-22 13:50:20 +0530

    Redux related additions for basic state management added in:
    1) Reducer for giving out book list
    2) Book selection List
    3) Promoting and book list component to an redux container for combining
    React and Redux
     ** Fixing formating for majority of the script files

---
+++ udemy/ReduxSimpleStarter/src/reducers/reducer_books.js
@@ -0,0 +1,19 @@
+export default function () {
+    return [
+        {
+            'title': 'Javascrip, The Good Parts'
+        },
+        {
+            'title': 'Harry Potter'
+        },
+        {
+            'title': 'The Dark Tower'
+        },
+        {
+            'title': 'Eloquent Ruby'
+        },
+        {
+            'title': 'Ego is the Enemy'
+        }
+    ];
+}
