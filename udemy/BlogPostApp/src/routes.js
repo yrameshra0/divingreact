f2e9ebda5ff4c3e047ea55ba8de0c30fb762da86
Ramesh <yrameshrao@gmail.com>
2017-01-04 04:55:03 +0530

    Adding index route and making blog posts list as default index component

--- udemy/BlogPostApp/src/routes.js
+++ udemy/BlogPostApp/src/routes.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 import App from './components/app';
+import PostIndex from './components/posts/index';
 
 export default (
-    <Route path="/" component={App}/>
-);
+    <Route path="/" component={App}>
+        <IndexRoute component={PostIndex}/>
+    </Route>);
