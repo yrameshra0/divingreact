9c54a48703520e075978020fd91bd99bd437a251
Ramesh <yrameshrao@gmail.com>
2017-01-06 19:09:16 +0530

    Clean up `

--- udemy/WeatherForecastApp/src/reducers/reducer_weather.js
+++ udemy/WeatherForecastApp/src/reducers/reducer_weather.js
@@ -2,7 +2,6 @@ import {FETCH_WEATHER}from '../actions/index';
 export default function (state = [], action) {
     switch (action.type) {
         case FETCH_WEATHER:
-
             return [action.payload.data, ...state];
     }
 
