5b19a7c448d3b76d9b1b15617b630a19b7b5fa43
Ramesh <yrameshrao@gmail.com>
2017-01-10 15:49:46 +0530

    Improvising the layout of the view

--- udemy/BlogPostApp/src/components/post/index.js
+++ udemy/BlogPostApp/src/components/post/index.js
@@ -8,34 +8,38 @@ class PostIndex extends Component {
         this.props.fetchPosts();
     }
 
-    renderPost = (post) => {
-        return (
-            <div key={post.id}>
-                <div>Title : {post.title}</div>
-                <div>Cateogries : {post.categories}</div>
-            </div>
+    renderPosts = (post) => {
+        return this.props.posts.map((post) => {
+                return (
+                    <li className="list-group-item" key={post.id}>
+                        <strong className="col-xs-12 col-md-8">{post.title}</strong>
+                        <span className="col-xs-6 col-md-4 text-xl-right">{post.categories}</span>
+                    </li>
+                );
+            }
         );
     };
 
     render() {
         return (
             <div>
-                <div className="text-xs-right">
+                <div className="text-xl-right">
                     <Link to="post/new" className="btn btn-primary">
                         Add new post
                     </Link>
                 </div>
 
-                List of blog posts :
-
-                {this.props.all.map(this.renderPost)}
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
             </div>
         );
     }
 }
 
 function mapStateToProps(state) {
-    return {all: state.posts.all};
+    return {posts: state.posts.all};
 }
 
 export default connect(mapStateToProps, {fetchPosts})(PostIndex);
