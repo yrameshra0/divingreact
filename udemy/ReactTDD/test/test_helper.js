10b3c28385bc1870bc5c1ae74f8d57f544481eaf
Ramesh <yrameshrao@gmail.com>
2017-02-24 05:26:26 +0530

    Bring up the test helper understanding the bits and pieces of it
    WIP 4UTs passing and 3Failing currently JSDOM + JQUERY + REACT Render
    component hooked up together.

--- udemy/ReactTDD/test/test_helper.js
+++ udemy/ReactTDD/test/test_helper.js
@@ -1,36 +1,28 @@
-import _$ from 'jquery';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
 import jsdom from 'jsdom';
-import chai, { expect } from 'chai';
-import chaiJquery from 'chai-jquery';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from '../src/reducers';
+import jquery from 'jquery';
+import TestUtils from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
+import {expect} from 'chai';
 
+// Setting up our test environment to run like a browser in command-line
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
-global.navigator = global.window.navigator;
-const $ = _$(window);
 
-chaiJquery(chai, chai.util, $);
+// Setting up jquery to use window we defined using jsdom
+const $ = jquery(global.window);
 
-function renderComponent(ComponentClass, props = {}, state = {}) {
-  const componentInstance =  TestUtils.renderIntoDocument(
-    <Provider store={createStore(reducers, state)}>
-      <ComponentClass {...props} />
-    </Provider>
-  );
+// build a 'renderComponent' helper that should render a given react class
+function renderComponent(componentClass){
+    const componentInstance = TestUtils.renderIntoDocument(<ComponentClass />);
 
-  return $(ReactDOM.findDOMNode(componentInstance));
+    // Produces HTML which is then wrapped to jquery so that we get elegant assertions on the testing side
+    return $(ReactDOM.findDOMNode(componentInstance));
+
 }
 
-$.fn.simulate = function(eventName, value) {
-  if (value) {
-    this.val(value);
-  }
-  TestUtils.Simulate[eventName](this[0]);
-};
+// Build a helper for simulating events
+
+// Set up chai-jquery
+
 
 export {renderComponent, expect};
