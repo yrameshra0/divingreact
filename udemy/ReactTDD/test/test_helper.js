faf41c39cc651818064ddb4d203fab84c809740e
Ramesh <yrameshrao@gmail.com>
2017-02-27 05:23:01 +0530

    Hooking of the redux store with with component rendering.
    State and props can be sent hover to the component from managing the
    data and its proper rendering.

--- udemy/ReactTDD/test/test_helper.js
+++ udemy/ReactTDD/test/test_helper.js
@@ -1,8 +1,12 @@
+import React from 'react';
 import jsdom from 'jsdom';
 import jquery from 'jquery';
 import TestUtils from 'react-addons-test-utils';
 import ReactDOM from 'react-dom';
 import {expect} from 'chai';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducers from '../src/reducers';
 
 // Setting up our test environment to run like a browser in command-line
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
@@ -12,8 +16,12 @@ global.window = global.document.defaultView;
 const $ = jquery(global.window);
 
 // build a 'renderComponent' helper that should render a given react class
-function renderComponent(componentClass){
-    const componentInstance = TestUtils.renderIntoDocument(<ComponentClass />);
+function renderComponent(ComponentClass, props, state) {
+    const componentInstance = TestUtils.renderIntoDocument(
+        <Provider store={createStore(reducers, state)}>
+            <ComponentClass {...props}/>
+        </Provider>
+    );
 
     // Produces HTML which is then wrapped to jquery so that we get elegant assertions on the testing side
     return $(ReactDOM.findDOMNode(componentInstance));
