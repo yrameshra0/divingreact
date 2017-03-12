c5e4e09e1ec3cbb53092fea83e36611126e7f7f0
Ramesh <ramesh@deepsydoodle.com>
2017-03-13 05:04:05 +0530

    Basic Header component setup completed

--- udemy/HOCHelpers/src/components/app.js
+++ udemy/HOCHelpers/src/components/app.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Header from './header';
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter</div>
-    );
-  }
+    render() {
+        return (
+            <Header/>
+        );
+    }
 }
