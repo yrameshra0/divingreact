96a02f2d5405b8295cca5478c79715a9f57b3809
Ramesh <ramesh@deepsydoodle.com>
2017-03-29 19:25:06 +0530

    Require authentication higher order component
    HOC for managing all the state around application user authetication so
    that our components don't end up dealing with all that mambo-gambo

--- udemy/HOCHelpers/src/index.js
+++ udemy/HOCHelpers/src/index.js
@@ -4,8 +4,8 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import {Router, Route, browserHistory} from 'react-router';
 import Resources from './components/resources';
-
 import App from './components/app';
+import requireAuth from './components/require_authentication';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
@@ -14,7 +14,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
       <Router history={browserHistory}>
           <Route path="/" component={App}>
-              <Route path="resources" component={Resources}/>
+              <Route path="resources" component={requireAuth(Resources)}/>
           </Route>
       </Router>
   </Provider>
